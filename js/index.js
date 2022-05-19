eb7c8ce272e62e23c819c34b1cccbefb6be28543
JharD-1 <104238288+JharD-1@users.noreply.github.com>
2022-05-19 10:07:03 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,51 +1,73 @@
-// EYE TOGGLER
-$(".toggle-password").click(function() {
+//VALIDATION
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
 
-  $(this).toggleClass("fa-eye fa-eye-slash");
-  var input = $($(this).attr("toggle"));
-  if (input.attr("type") == "password") {
-    input.attr("type", "text");
-  } else {
-    input.attr("type", "password");
-  }
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
 });
 
-// VALIDATIONS
-const usernameEl = document.querySelector('#username');
-const emailEl = document.querySelector('#email');
-const passwordEl = document.querySelector('#password');
-const confirmPasswordEl = document.querySelector('#confirm-password');
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
 
-const form = document.querySelector('#signup');
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
 
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
 
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
 
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
 
-document.getElementById("myForm").addEventListener("submit", function(evt) {
-    evt.preventDefault();
-    var error = "";
-    if (document.getElementById("username").value == "") {
-        error += "The username field is required.<br>";
-      }
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
 
-    if (document.getElementById("email").value == "") {
-      error += "The email field is required.<br>";
-    }
-  
-    if (document.getElementById("password").value == "") {
-      error += "Password is required.<br>";
-    }
-
-    if (document.getElementById("confirm-password").value == "") {
-        error += "Confirm Password is required.<br>";
-      }
-  
-    if (error != "") {
-      document.getElementById("error").innerHTML = '<div class="alert alert-danger" role="alert"><p><strong>There were error(s) in your form:</strong></p>' + error + '</div>';
-      return false;
+    if(usernameValue === '') {
+        setError(username, 'Username is required');
     } else {
-      document.getElementById("myForm").removeEventListener("submit", function() {
-        return true;
-      });
+        setSuccess(username);
     }
-  });
+
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+
+    if(passwordValue === '') {
+        setError(password, 'Password is required');
+    } else if (passwordValue.length < 8 ) {
+        setError(password, 'Password must be at least 8 character.')
+    } else {
+        setSuccess(password);
+    }
+
+    if(password2Value === '') {
+        setError(password2, 'Please confirm your password');
+    } else if (password2Value !== passwordValue) {
+        setError(password2, "Passwords doesn't match");
+    } else {
+        setSuccess(password2);
+    }
+
+};
